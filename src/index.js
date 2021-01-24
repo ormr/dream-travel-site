3af3910dc5a9d0a340e1aa40fc1c63b8f282af14
Serafim <seraf.gavrilov@gmail.com>
2021-01-24 23:04:55 +0300

    Add listener onload for sliders

--- src/index.js
+++ src/index.js
@@ -4,95 +4,100 @@ import 'swiper/swiper-bundle.css';
 
 Swiper.use([Navigation, Pagination]);
 
-const swiper1 = new Swiper('.popular-carousel', {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  slidesPerGroup: 1,
-  loopFillGroupWithBlank: true,
-  centeredSlides: true,
-  loop: true,
-  // speed: 3000,
-  pagination: {
-    el: '.popular-carousel__pagination',
-    clickable: true,
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 3,
+window.addEventListener(
+  'load',
+  () => {
+    const swiper1 = new Swiper('.popular-carousel', {
+      slidesPerView: 1,
       spaceBetween: 30,
-      slidesPerGroup: 3,
-    },
-  },
-});
+      slidesPerGroup: 1,
+      loopFillGroupWithBlank: true,
+      centeredSlides: true,
+      loop: true,
+      pagination: {
+        el: '.popular-carousel__pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+          slidesPerGroup: 3,
+        },
+      },
+    });
 
-setInterval(() => swiper1.slideNext(), 7000);
+    setInterval(() => swiper1.slideNext(), 7000);
 
-const swiper2 = new Swiper('.most-popular-slider', {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: '.most-popular-slider__pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.most-popular-slider__next-button',
-    prevEl: '.most-popular-slider__prev-button',
-  },
-});
+    const swiper2 = new Swiper('.most-popular-slider', {
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.most-popular-slider__pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.most-popular-slider__next-button',
+        prevEl: '.most-popular-slider__prev-button',
+      },
+    });
 
-setInterval(() => swiper2.slideNext(), 7000);
+    setInterval(() => swiper2.slideNext(), 7000);
 
-const swiper3 = new Swiper('.honeymoon-offer-slider', {
-  autoHeight: 'true',
-  slidesPerView: 'auto',
-  spaceBetween: 30,
-  slidesPerGroup: 1,
-  loop: true,
-  pagination: {
-    el: '.honeymoon-offer-slider__pagination',
-    clickable: true,
-  },
-  breakpoints: {
-    768: {
+    const swiper3 = new Swiper('.honeymoon-offer-slider', {
       autoHeight: 'true',
       slidesPerView: 'auto',
       spaceBetween: 30,
-      slidesPerGroup: 3,
-    },
+      slidesPerGroup: 1,
+      loop: true,
+      pagination: {
+        el: '.honeymoon-offer-slider__pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        768: {
+          autoHeight: 'true',
+          slidesPerView: 'auto',
+          spaceBetween: 30,
+          slidesPerGroup: 3,
+        },
+      },
+    });
+
+    setInterval(() => swiper3.slideNext(), 7000);
+
+    const swiper4 = new Swiper('.most-popular-slider', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.most-popular-slider__pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.most-popular-slider__next-button',
+        prevEl: '.most-popular-slider__prev-button',
+      },
+    });
+
+    setInterval(() => swiper4.slideNext(), 14000);
+
+    const swiper5 = new Swiper('.ticket-slider', {
+      slidesPerView: 'auto',
+      spaceBetween: 200,
+      slidesPerGroup: 1,
+      centeredSlides: true,
+      navigation: {
+        nextEl: '.ticket-slider__next-button',
+        prevEl: '.ticket-slider__prev-button',
+      },
+    });
+
+    setInterval(() => swiper5.slideNext(), 14000);
   },
-});
-
-setInterval(() => swiper3.slideNext(), 7000);
-
-const swiper4 = new Swiper('.most-popular-slider', {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: '.most-popular-slider__pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.most-popular-slider__next-button',
-    prevEl: '.most-popular-slider__prev-button',
-  },
-});
-
-setInterval(() => swiper4.slideNext(), 14000);
-
-const swiper5 = new Swiper('.ticket-slider', {
-  slidesPerView: 'auto',
-  spaceBetween: 200,
-  slidesPerGroup: 1,
-  centeredSlides: true,
-  navigation: {
-    nextEl: '.ticket-slider__next-button',
-    prevEl: '.ticket-slider__prev-button',
-  },
-});
-
-setInterval(() => swiper5.slideNext(), 14000);
+  false
+);
 
 const toggleMobileNav = () => {
   const mobileNav = document.querySelector('.mobile-nav');
